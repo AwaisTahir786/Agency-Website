9c488b1f04e42ff3f9ae225db99c68d54495ede6
AwaisTahir786 <tahirawais111@gmail.com>
2024-04-16 03:46:28 +0500

    push

--- src/components/Testmonials.tsx
+++ src/components/Testmonials.tsx
@@ -15,45 +15,45 @@ function Test() {
       </div> 
 
       <div className="container mx-auto mb-10 lg:px-10">
-        <div className="grid gap-6 max-w-screen px-24 lg:grid-cols-2 xl:grid-cols-3">
+        <div className="grid gap-6 max-w-screen px-40 lg:grid-cols-2 xl:grid-cols-3">
             <div className="">
             <div className="flex flex-col justify-between w-full h-[20rem]	 px-6 py-6 bg-gray-100 dark:bg-gray-800 md:px-10 rounded-2xl md:py-10   dark:bg-trueGray-800">
-                    <p className="text-2xl leading-normal">Share a<mark className="mx-1 text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">testimonial</mark>that hits some of your benefits from one of your popular customer.</p>
+                    <p className="text-xl leading-normal">Share a<mark className="mx-1 text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">testimonial</mark>that hits some of your benefits from one of your popular customer.</p>
                     <div className="flex items-center mt-8 space-x-3">
                         <div className="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
                             <Image alt="Avatar" src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?crop=faces&amp;cs=tinysrgb&amp;fit=crop&amp;fm=jpg&amp;ixid=MnwxfDB8MXxhbGx8fHx8fHx8fHwxNjIwMTQ5ODEx&amp;ixlib=rb-1.2.1&amp;q=80&amp;w=100&amp;h=100" loading="lazy" height={100} width={100}/>
                         </div>
                         <div>
                             <div className="text-lg font-medium">Sarah Steiner</div>
-                            <div className="text-gray-600 dark:text-gray-400">VP Sales at Google</div>
+                            <div className="text-gray-600 text-sm dark:text-gray-400">VP Sales at Google</div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="">
             <div className="flex flex-col justify-between w-full h-[20rem]	 px-6 py-6 bg-gray-100 dark:bg-gray-800 md:px-10 rounded-2xl md:py-10   dark:bg-trueGray-800">
-                    <p className="text-2xl leading-normal">Make sure you only pick the <mark className="mx-1 text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">right sentence</mark> to keep it short and simple.</p>
+                    <p className="text-xl leading-normal">Make sure you only pick the <mark className="mx-1 text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">right sentence</mark> to keep it short and simple.</p>
                     <div className="flex items-center mt-8 space-x-3">
                         <div className="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
                             <Image alt="Avatar" src="https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;crop=faces&amp;fit=crop&amp;w=100&amp;h=100&amp;q=80" loading="lazy" height={100} width={100} />
                         </div>
                         <div>
                             <div className="text-lg font-medium">Dylan Ambrose</div>
-                            <div className="text-gray-600 dark:text-gray-400">Lead marketer at Netflix </div>
+                            <div className="text-gray-600 text-sm dark:text-gray-400">Lead marketer at Netflix </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="">
             <div className="flex flex-col justify-between w-full h-[20rem]	 px-6 py-6 bg-gray-100 dark:bg-gray-800 md:px-10 rounded-2xl md:py-10   dark:bg-trueGray-800">
-                    <p className="text-2xl leading-normal">This is an <mark className="mx-1 text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">awesome</mark> landing page template I&#x27;ve seen. I would use this for anything.</p>
+                    <p className="text-xl leading-normal">This is an <mark className="mx-1 text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">awesome</mark> landing page template I&#x27;ve seen. I would use this for anything.</p>
                     <div className="flex items-center mt-8 space-x-3">
-                        <div className="flex-shrink-0 overflow-hidden rounded-full w-20 h-20">
-                            <Image alt="Avatar" src="https://images.unsplash.com/photo-1624224971170-2f84fed5eb5e?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=100&amp;h=100&amp;crop=faces&amp;q=80" loading="lazy" height={50} width={50}/>
+                        <div className="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
+                            <Image alt="Avatar" src="https://images.unsplash.com/photo-1624224971170-2f84fed5eb5e?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=100&amp;h=100&amp;crop=faces&amp;q=80" loading="lazy" height={100} width={100} />
                         </div>
                         <div>
                             <div className="text-lg font-medium">Gabrielle Winn</div>
-                            <div className="text-gray-600 dark:text-gray-400">Co-founder of Acme Inc</div>
+                            <div className="text-gray-600 text-sm dark:text-gray-400">Co-founder of Acme Inc</div>
                         </div>
                     </div>
                 </div>
@@ -62,42 +62,42 @@ function Test() {
         
             <div className="">
             <div className="flex flex-col justify-between w-full h-[20rem]	 px-6 py-6 bg-gray-100 dark:bg-gray-800 md:px-10 rounded-2xl md:py-10   dark:bg-trueGray-800">
-                    <p className="text-2xl leading-normal">Share a<mark className="mx-1 text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">testimonial</mark>that hits some of your benefits from one of your popular customer.</p>
+                    <p className="text-xl leading-normal">Share a<mark className="mx-1 text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">testimonial</mark>that hits some of your benefits from one of your popular customer.</p>
                     <div className="flex items-center mt-8 space-x-3">
                         <div className="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
                             <Image alt="Avatar" src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?crop=faces&amp;cs=tinysrgb&amp;fit=crop&amp;fm=jpg&amp;ixid=MnwxfDB8MXxhbGx8fHx8fHx8fHwxNjIwMTQ5ODEx&amp;ixlib=rb-1.2.1&amp;q=80&amp;w=100&amp;h=100" loading="lazy" height={100} width={100} />
                         </div>
                         <div>
                             <div className="text-lg font-medium">Sarah Steiner</div>
-                            <div className="text-gray-600 dark:text-gray-400">VP Sales at Google</div>
+                            <div className="text-gray-600 text-sm dark:text-gray-400">VP Sales at Google</div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="">
             <div className="flex flex-col justify-between w-full h-[20rem]	 px-6 py-6 bg-gray-100 dark:bg-gray-800 md:px-10 rounded-2xl md:py-10   dark:bg-trueGray-800">
-                    <p className="text-2xl leading-normal">Make sure you only pick the <mark className="mx-1 text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">right sentence</mark> to keep it short and simple.</p>
+                    <p className="text-xl leading-normal">Make sure you only pick the <mark className="mx-1 text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">right sentence</mark> to keep it short and simple.</p>
                     <div className="flex items-center mt-8 space-x-3">
                         <div className="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
                             <Image alt="Avatar" src="https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;crop=faces&amp;fit=crop&amp;w=100&amp;h=100&amp;q=80" loading="lazy" height={100} width={100} />
                         </div>
                         <div>
                             <div className="text-lg font-medium">Dylan Ambrose</div>
-                            <div className="text-gray-600 dark:text-gray-400">Lead marketer at Netflix </div>
+                            <div className="text-gray-600 text-sm dark:text-gray-400">Lead marketer at Netflix </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="">
             <div className="flex flex-col justify-between w-full h-[20rem]	 px-6 py-6 bg-gray-100 dark:bg-gray-800 md:px-10 rounded-2xl md:py-10   dark:bg-trueGray-800">
-                    <p className="text-2xl leading-normal">This is an <mark className="mx-1 text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">awesome</mark> landing page template I&#x27;ve seen. I would use this for anything.</p>
+                    <p className="text-xl leading-normal">This is an <mark className="mx-1 text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">awesome</mark> landing page template I&#x27;ve seen. I would use this for anything.</p>
                     <div className="flex items-center mt-8 space-x-3">
                         <div className="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
                             <Image alt="Avatar" src="https://images.unsplash.com/photo-1624224971170-2f84fed5eb5e?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=100&amp;h=100&amp;crop=faces&amp;q=80" loading="lazy" height={100} width={100} />
                         </div>
                         <div>
                             <div className="text-lg font-medium">Gabrielle Winn</div>
-                            <div className="text-gray-600 dark:text-gray-400">Co-founder of Acme Inc</div>
+                            <div className="text-gray-600 text-sm dark:text-gray-400">Co-founder of Acme Inc</div>
                         </div>
                     </div>
                 </div>
